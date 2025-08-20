2f7491fdf11e49206d0d975360acc9f284f3230a
Benjamin Stevens <B3nbinoh@gmail.com>
2025-08-20 13:58:15 -0600

    Removed unnecesarry route handling from static app

--- static/app.js
+++ static/app.js
@@ -6,10 +6,6 @@ const PORT = 3000;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile("/index");
-});
-
 app.listen(PORT, (req, res) => {
   console.log(`Your app is now running on port: ${PORT}`);
 });
